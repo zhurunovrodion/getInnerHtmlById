ee34fef6372b8e5678d6b76e35cbec2296f6e4fe
zhurunov.rodion <zhurunov.rodion@yandex.ru>
2018-05-02 11:43:02 +0500

    update

--- getInnerHTMLById.js
+++ getInnerHTMLById.js
@@ -25,7 +25,7 @@ function getInnerHTMLById(htmlString, id) {
 						if(currentTag.match(openTagRegExp)) {
 							if(!singleTagCheck(currentTag)) {
 								countOfOpenTags++;
-							}
+							} 
 						}
 						if(currentTag.match(closeTagRegExp)) {
 							countOfCloseTags++;
@@ -67,6 +67,7 @@ function getInnerHTMLById(htmlString, id) {
 		return false;
 	}
 }
+
 
 var testHtmlString = '<div class="showNews"><div class="breadCrumbWrapper textStandart m--breadCrumbWrapper-withPadding"><div  class="breadCrumb"><ul class="breadCrumb__list"><li class="breadCrumb__element"> <a href="/">радио 100</a></li><li class="breadCrumb__element"> <a href="/novosti/">Новости</a></li></ul></div></div><div class="twoColumsContent"><div class="twoColumsContent__wrapper"><div   class="twoColumsContent__firstColumn"><div class="oneColumnHeader textStandart"><h1>Группа ABBA записала новые песни</h1></div><div id="test2" class="headerImageBlock"> <img src="/files/images/2804abba.jpg" alt="Изображение новости"></div></div><div class="twoColumsContent__secondColumn m--secondColumn-bgGrey"><div  class="textBlockContent textStandart"><div class="dateBlock "> 2018/04/28</div><p>Шведский квартет ABBA анонсировал выход двух новых песен, сообщается в&nbsp;Instagram&nbsp;музыкантов.</p><p>«Мы вчетвером почувствовали, что после 35 лет будет весело снова собраться и поработать в звукозаписывающей студии. И мы сделали это. И нам показалось, что ничего как будто не поменялось за все это время, а мы всего лишь ушли в короткий отпуск», — говорится в посте.</p><p >Участники группы объявили, что им удалось записать две песни. Одну из композиций, которая получила название I Still Have Faith In You, проиграют по телевидению в декабре. Релизом займутся&nbsp;«Би-би-си»&nbsp;и&nbsp;NBC. «Мы, может, уже совсем зрелые, но песня — новая. И это здорово», — отметили музыканты.</p><p>В октябре 2016 года ABBA объявила о воссоединении. Последнее официальное выступление группы состоялось в 1982-м, однако в июне 2016 года ABBA сыграла на частном гала-вечере в Стокгольме.</p><p>&nbsp;</p><p>Фото:&nbsp;<a href="https://www.newsmax.com/" target="_blank">newsmax.com</a></p></div></div></div></div></div>';
 console.log(getInnerHTMLById(testHtmlString, 'test2')); 
